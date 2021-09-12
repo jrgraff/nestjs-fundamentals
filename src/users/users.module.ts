18ca42a7c18d2c71e7fb74fabac7937a0045876a
Jackson Graff <jacksongraff@hotmail.com>
2021-09-12 17:05:35 -0300

    chore: initial settings

---
+++ src/users/users.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+
+import { UsersController } from './controllers/users/users.controller';
+import { PostsController } from './controllers/posts/posts.controller';
+import { UserService } from './service/user/user.service';
+import { PostService } from './service/post/post.service';
+
+@Module({
+  controllers: [UsersController, PostsController],
+  providers: [UserService, PostService],
+})
+export class UsersModule {}
